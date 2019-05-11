66a8a739a78560c280fee75477151ee900a06c99
Joseph Millan <https://github.com/JMillan13>
2019-05-11 16:58:22 -0400

    another update

--- Bamazon.js
+++ Bamazon.js
@@ -1,11 +1,7 @@
 
 var mysql = require("mysql");
-var Table = require("cli-table2");
 var inquirer = require("inquirer");
 
-var displayTable = require("./displayConstructor.js");
-
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -16,86 +12,89 @@ var connection = mysql.createConnection({
   user: "root",
 
   // Your password
-  password: "",
+  password: "ajmillan13",
   database: "BAMAZON_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+  displayForCustomer();
 });
 
 //Display db using a table from npm pack cli-table2
 // See if Customer would want to purchase item and how many
-var displayForCustomer = function() {
-    var display = new displayTable();
-    connection.query("Select *, from products", function(err, results){
-      display.displayIventoryTable(results);
-      purchaseItem();
-    });
+var displayForCustomer = function () {
+  // var display = new displayTable();
+  connection.query("Select * from products", function (err, results) {
+    if (err) throw err;
+    // display.displayIventoryTable(results);
+    console.log(results);
+    purchaseItem();
+  });
 
 }
 
-//Enter Customer Name and amount to wish they would like ot purchase
-var purchaseItem = function() {
-  console.log ('\n');
+var purchaseItem = function () {
   inquirer.prompt([{
-        name: "quantity",
-        type: "input",
-        message: " Enter the amount you want to purchase",
-  }]) .then(function(answer) {
+    name: "id",
+    type: "input",
+    message: " Enter the id of the item you want to buy",
+  },{
+    name: "quantity",
+    type: "input",
+    message: " Enter the amount you want to purchase",
+  }])
+  .then(function (answer) {
     // Query the DB for info on item and quantity
-    Connection.query("Select ProductName, DepartName, Price, StockQuantity from products where?" , {ItemID: answer.id} function(err,res){
+    connection.query("Select product_name, department_name, price, stock_quantity from products where?", { item_id: answer.id }
+      , function (err, res) {
+          if (err) throw err;
 
-      console.log('\n You would like to buy ' + answer.quantity + ' ' +) res[0].ProductName + ' ' + res[0].DepartmentName + ' at $' + res[0].Price + ' each'
-			);
+          if (res[0].StockQuantity >= answer.quantity) {
+            //If enough inventory to complete order, process order by updating database inventory and notifying customer that order is complete.
+            var itemQuantity = res[0].StockQuantity - answer.quantity;
 
-      if (res[0].StockQuantity >= answer.quantity) {
-        //If enough inventory to complete order, process order by updating database inventory and notifying customer that order is complete.
-        var itemQuantity = res[0].StockQuantity - answer.quantity;
-        connection.query("UPDATE products SET ? WHERE ?",[{
-        {
-          StockQuantity: itemQuantity
+            connection.query("UPDATE products SET ? WHERE ?",
+              [
+                { stock_quantity: itemQuantity},
+                { item_id: answer.id}
+              ],
+              function(err, res) {
+                //notify user after db is updated
+                var cost = res[0].Price * answer.quantity;
+                console.log('\n  Order fulfilled! Your cost is $' + cost.toFixed(2) + '\n');
+                // Order completed
+                customerPrompt();
+              }
+            );
+          } else {
+            //If not enought inventory notify customer and prompt customer for desire to shop more
+            console.log('\n  Sorry, Insufficient quantity to fulfill your order!\n');
+            // Order not completed
+            customerPrompt();
+          }
+      }
+    )
+  });
+}
 
-    }
-    ItemID: answer.id
-				}], function(err,res) {
-          });
-        {
-				var cost = res[0].Price * answer.quantity;
-				console.log('\n  Order fulfilled! Your cost is $' + cost.toFixed(2) + '\n');
-				// Order completed
-				customerPrompt();
-      } else {
-				//If not enought inventory notify customer and prompt customer for desire to shop more
-				console.log('\n  Sorry, Insufficient quantity to fulfill your order!\n');
-				// Order not completed
-				customerPrompt();
-			}
-    })
-  };
 
-
-var customerPrompt = function() {
-    inquirer.prompt({
-        name: "action",
-        type: "list",
-
-        message: " Would like to continue shopping?\n",
-        choices: ["Yes", "No"]
-    }).then(function(answer) {
-        switch(answer.action) {
-            case 'Yes':
-                displayForUser();
-            break;
-            case 'No':
-            connection.end();
+var customerPrompt = function () {
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: " Would like to continue shopping?\n",
+    choices: ["Yes", "No"]
+  })
+  .then(function (answer) {
+    switch (answer.action) {
+      case 'Yes':
+        displayForUser();
+        break;
+      case 'No':
+        connection.end();
         break;
     }
-})
+  })
 };
-
-// Start app by Prompting the customer
-customerPrompt();
--- Bamazon.sql
+++ Bamazon.sql
@@ -1,1 +1,17 @@
+
+DROP DATABASE IF EXISTS BAMAZON_DB;
 CREATE DATABASE BAMAZON_DB;
+USE BAMAZON_DB;
+
+CREATE TABLE products (
+	item_id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+	department_name VARCHAR(100) NOT NULL,
+    price FLOAT(10, 2) NOT NULL,
+    stock_quantity INTEGER NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+INSERT INTO products ( product_name, department_name, price, stock_quantity)
+VALUES ("Pencil", "School Supplies", 1.00, 100),
+	("Eraser", "School Supplies", 0.50, 100)
